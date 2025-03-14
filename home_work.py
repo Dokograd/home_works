7d239318f3283902ddfedce31e95830f6030a503
Dokograd <dokongrad@mail.ru>
2025-03-13 21:09:27 -0700

    bul tema set

---
+++ home_work.py
@@ -0,0 +1,17 @@
+# set - mnojestvo
+nums = {1,1,1,2,2,2,3,3,3}
+names = {
+    "arsen",
+    "grad",
+    "kanat",
+    "shera",
+    "ernis",
+    "aida",
+    "ayna",
+    "begimai",
+}
+name = input("enter a name ")
+print(names)
+names.add(name)
+print(names)
+
