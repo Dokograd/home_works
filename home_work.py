b8853f47e4ffd8f2ee0e8585edbca1e11cb49e23
Dokograd <dokongrad@mail.ru>
2025-03-13 22:00:46 -0700

    fuksiy jonundo tema

---
+++ home_work.py
@@ -0,0 +1,36 @@
+def func():
+    print("python")
+    print("java")
+    print("c++")
+    print("#" * 10)
+
+def area_circle():
+    radius = float(input("enter the radius"))
+    s = 3.14 * radius ** 2
+    print(" aynt = ", s)
+area_circle()
+
+
+def area_rectangle(width, height):
+    if width < 0 or height < 0:
+        print("tort burchtuktyn jaktary ters bolboit")
+    else:
+        area = width * height
+    area = width * height
+    print("ayant=", area)
+area_circle()
+area_rectangle(100,100)
+
+def name_get(name):
+    print(f'yor names {name}')
+name_get("dokon")
+
+aty = input("enter your name")
+name_get(aty)
+
+
+
+
+
+
+
